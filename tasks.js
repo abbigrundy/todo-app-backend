b842734b644c96bf5f9cb82996f1d157b914dc66
Abigail Grundy <abigailgrundy@Abigails-MBP.connect>
2019-02-07 13:04:27 +0000

    Added DELETE handler:with taskID. POST function created.

--- tasks.js
+++ tasks.js
@@ -4,9 +4,7 @@ const app = express();
 
 app.get('/tasks', function (request, response) {
 
-  const username = request.query.username;
-
-  const arrayoftasks = [
+  const someTasks = [
   {
             id: 1,
             description:"Buy a bottle of milk",
@@ -23,7 +21,38 @@ app.get('/tasks', function (request, response) {
           completed: false
   }
 ];
-  response.json(arrayoftasks);
-})
+  response.json(someTasks);
+});
 
+app.delete('/tasks/:taskId', function (request, response) {
+
+  const taskIdToBeDeleted =  request.params.taskId;
+
+  let someResponse = {
+    message: "You issued a delete request for ID:" + taskIdToBeDeleted
+  };
+
+  if(taskIdToBeDeleted > 3 ) {
+    response.status(404);
+    someResponse = {
+      message: "Task:" + taskIdToBeDeleted + "does not exist"
+    };
+  }
+
+    response.json(someResponse);
+ 
+  });
+
+  app.post('/tasks/:newTask', function (request, response) {
+    const taskAdded = [ {
+      id : Number,
+      description: " ",
+      completed: false,
+  }]
+  let someResponse = {
+    message: "You issued a new task" + taskAdded
+  };
+  response.json(someResponse); 
+
+  });
 module.exports.handler = serverless(app);
